288a588fbb73cc016038085fdf6f150152a70ff6
Benjamin Dendas <benjamin.dendas@craftworkz.be>
2020-04-09 15:31:32 +0200

    KAS-1322: remove dependencies + use prefixes in query

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 import mu from 'mu';
 
 const app = mu.app;
-const bodyParser = require('body-parser');
 const repository = require('./repository');
-const cors = require('cors');
-const debug = process.env.DEBUG_LOGGING || false;
-
-app.use(bodyParser.json({ type: 'application/*+json' }));
-app.use(cors());
-
 
 app.get('/', function( req, res ) {
   res.send('Hello mu-javascript-template');
